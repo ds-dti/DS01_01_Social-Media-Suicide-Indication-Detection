5030a0a5394365dd3daabf529ff5440d3925cbf8
Dimas Samodra <48814594+DSamodra@users.noreply.github.com>
2020-12-05 16:00:54 +0700

    Example Web App Machine Learning on flask

---
+++ Deploy Web App/app.py
@@ -0,0 +1,31 @@
+# import library
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# define name and load model
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+# main route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# predic route
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
